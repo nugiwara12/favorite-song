5c23ad1ccc4ecbeebb003a2a38a60ae96f24bf65
Nugiwara_011200 <global-config@example.com>
2024-06-14 18:14:28 +0800

    add all data

--- src/app/page.js
+++ src/app/page.js
@@ -2,112 +2,192 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="container mx-auto text-white px-4 py-16">
+      <h1 className="text-header font-bold mb-4 text-center">
+        Play your favorite songs.
+      </h1>
+      <p className="text-lg text-center mb-8">
+        You’ll have all the tools you need to make sure you never miss a beat.
+        <a
+          href="#"
+          className="font-bold text-red-600 underline decoration-red-600"
+        >
+          Try the demo
+        </a>
+      </p>
+      <div className="flex flex-col lg:flex-row justify-center items-center">
+        <div className="flex justify-center items-center w-full mx-auto">
+          <div className="flex flex-col items-center md:flex-row">
+            <div className="space-y-6">
+              <a
+                href="#"
+                className="flex items-start space-x-4 bg-element rounded-lg p-4 active-button"
+                onclick="openDescription('../playlist-video/playlist.mp4', 'description', 'anotherDescription', 'troubleSpotId', 'timingId', 'desktopId'); setActiveButton('showDescriptionButton');"
+                id="showDescriptionButton"
+              >
+                <div className="text-red-500 text-2xl flex-shrink-0 w-7">
+                  <Image
+                    src="./icons/music.svg"
+                    width={50}
+                    height={50}
+                    alt="playlist"
+                  />
+                </div>
+                <div className="flex flex-col flex-grow">
+                  <h2 className="text-xl font-bold">500+ popular songs.</h2>
+                  <p
+                    className="description mx-0 mt-1 sm:mt-2 text-sm overflow-hidden max-h-0 hidden"
+                    id="description"
+                  >
+                    Get note-for-note song breakdowns for every style, era, and
+                    skill level.
+                  </p>
+                </div>
+              </a>
+
+              <a
+                href="#"
+                className="flex items-start space-x-4 rounded-lg p-4"
+                onclick="openDescription('../playlist-video/tempo.mp4', 'anotherDescription', 'description', 'troubleSpotId', 'timingId', 'desktopId'); setActiveButton('showAnotherDescriptionButton');"
+                id="showAnotherDescriptionButton"
+              >
+                <div className="text-red-500 text-2xl flex-shrink-0 w-7">
+                  <Image
+                    src="./icons/tempo.svg"
+                    width={50}
+                    height={50}
+                    alt="Perfect Tempo"
+                  />
+                </div>
+                <div className="flex flex-col flex-grow">
+                  <h2 className="text-xl font-bold">Find the perfect tempo.</h2>
+                  <p
+                    className="description hidden mx-0 mt-1 sm:mt-2 text-sm overflow-hidden max-h-0"
+                    id="anotherDescription"
+                  >
+                    Slow down any section of a song to make those tricky bars
+                    easier.
+                  </p>
+                </div>
+              </a>
+
+              <a
+                href="#"
+                className="flex items-start space-x-4 rounded-lg p-4"
+                onclick="openDescription('../playlist-video/troublespot.mp4', 'troubleSpotId', 'description', 'anotherDescription', 'timingId', 'desktopId'); setActiveButton('showTroubleSpotIdButton');"
+                id="showTroubleSpotIdButton"
+              >
+                <div className="text-red-500 text-2xl flex-shrink-0 w-7">
+                  <Image
+                    src="./icons/troublespot.svg"
+                    width={50}
+                    height={50}
+                    alt="Perfect Tempo"
+                  />
+                </div>
+                <div className="flex flex-col flex-grow">
+                  <h2 className="text-xl font-bold">Loop the trouble spots.</h2>
+                  <p
+                    className="description hidden mx-0 mt-1 sm:mt-2 text-sm overflow-hidden max-h-0"
+                    id="troubleSpotId"
+                  >
+                    Create practice loops to play-through those difficult parts
+                    over and over.
+                  </p>
+                </div>
+              </a>
+
+              <a
+                href="#"
+                className="flex items-start space-x-4 rounded-lg p-4"
+                onclick="openDescription('../playlist-video/timing.mp4', 'timingId', 'description', 'anotherDescription', 'troubleSpotId', 'desktopId'); setActiveButton('showTimingIdButton');"
+                id="showTimingIdButton"
+              >
+                <div className="text-red-500 text-2xl flex-shrink-0 w-7">
+                  <Image
+                    src="./icons/timing.svg"
+                    width={50}
+                    height={50}
+                    alt="Perfect Tempo"
+                  />
+                </div>
+                <div className="flex flex-col flex-grow">
+                  <h2 className="text-xl font-bold">Improve your timing.</h2>
+                  <p
+                    className="description hidden mx-0 mt-1 sm:mt-2 text-sm overflow-hidden max-h-0"
+                    id="timingId"
+                  >
+                    Use the built-in metronome – your new best friend for
+                    difficult rhythms.
+                  </p>
+                </div>
+              </a>
+
+              <a
+                href="#"
+                className="flex items-start space-x-4 rounded-lg p-4"
+                id="showDesktopIdButton"
+              >
+                <div className="text-red-500 text-2xl flex-shrink-0">
+                  <Image
+                    src="./icons/desktop.svg"
+                    width={50}
+                    height={50}
+                    alt="Desktop Access"
+                  />
+                </div>
+                <div className="flex flex-col flex-grow">
+                  <h2 className="text-xl font-bold">
+                    Take your songs anywhere.
+                  </h2>
+                  <p
+                    className="description hidden mx-0 mt-1 sm:mt-2 text-sm overflow-hidden max-h-0"
+                    id="desktopId"
+                  >
+                    Accessible on any device, or printable, so you can play any
+                    song, any time.
+                  </p>
+                </div>
+              </a>
+            </div>
+            <div className="color-element w-96 text-white flex justify-end items-end ml-4 mt-6 md:mt-0">
+              <div className="flex flex-col md:flex-row items-center justify-center gap-4 p-4">
+                <div className="relative sm:order-1 rounded-l-xl overflow-hidden py-10 pl-10 max-w-xs sm:max-w-full">
+                  <video id="videoPlayer" width="490" autoPlay muted loop>
+                    <source
+                      id="videoSource"
+                      src="./playlist-video/playlist.mp4"
+                      type="video/mp4"
+                      className="w-full hidden media-toggle rounded-l-xl overflow-hidden"
+                    />
+                  </video>
+                  <Image
+                    id="imageElement"
+                    src=""
+                    width={100}
+                    height={100}
+                    alt="Media Display"
+                    className="media-toggle hidden rounded-l-xl overflow-hidden"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
+      <div className="flex justify-center mt-12">
+        <a href="#" className="w-96 button button-outline">
+          SEE SONGS LIST
         </a>
-
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="#"
+          className="w-96 flex items-center ml-4 p-2 button button-solid "
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          START FOR FREE
+          <i className="bi bi-arrow-right arrow ml-2"></i>
         </a>
       </div>
-    </main>
+      <p className="note">Songs included with Pianote+</p>
+    </div>
   );
 }
